653f576c4490254ad2d8d15df5f07193fda0db77
shinjw3360 <jeong36023610@gmail.com>
2025-05-09 10:24:36 +0900

    shinjw_0508_ver1

--- js/app.js
+++ js/app.js
@@ -21,15 +21,32 @@ const navContents = {
   subMenu: 'menu item',
 };
 
-// const nav = document.querySelector('nav ul');
+const nav = document.querySelector('nav ul');
 
-// //삽입될 요소 초기화 변수
-// let navList = '';
-// navContents.title.map((content, index) => {
-//   //누적시키기 : +=
-//   navList += `<li><a href="#">${content}</a></li>`;
-// });
+//삽입될 요소 초기화 변수
+let navList = '';
+navContents.title.map((content, index) => {
+  //누적시키기 : +=
+  navList += `              
+              <li class="hover_mouse">
+                <a href="#">${content}</a>
+                <div class="thumb1_imgbox">
+                  <h2>${content}</h2>
+                  <div class="thumb1_img_view">
+                    <img src="${navContents.img[index]}" alt="썸네일 이미지">
+                  </div>
+                  <ul class="sub_menu_item_list">
+                    <li><a href="#">${navContents.subMenu}</a></li>
+                    <li><a href="#">${navContents.subMenu}</a></li>
+                    <li><a href="#">${navContents.subMenu}</a></li>
+                    <li><a href="#">${navContents.subMenu}</a></li>
+                  </ul>
+                </div>
+              </li>`;
+});
 
-// //연산자 확인하기
+//질문에 대한 답변 :
 
-// nav.insertAdjacentHTML('beforeend', navList);
+//연산자 확인하기
+
+nav.insertAdjacentHTML('beforeend', navList);
